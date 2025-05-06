e5ac82f7610e8ec7c355d83db9f747bbe3eb4f3a
Erick Diaz Cerdas <erick.diaz@babelgroup.com>
2025-05-06 15:38:59 -0600

    update pipeline with build an eslint

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -5,6 +5,6 @@ describe('Pokedex', function () {
     cy.visit('http://localhost:5000')
     cy.contains('pikachu')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
-    })
   })
+})
   
