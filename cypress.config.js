286a495b1ccdc85a6022ab3fe54913eb2f7ad262
Erick Diaz Cerdas <erick.diaz@babelgroup.com>
2025-05-07 11:30:33 -0600

    update pipelines cypress github action v6 test 3

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,4 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  video: false,
-  e2e: {
-    baseUrl: 'http://localhost:5000',
-    setupNodeEvents(on, config) {
-      // puedes configurar plugins si es necesario
-    },
-  },
-})
+module.exports = {
+  e2e: {},
+  video: false
+}
